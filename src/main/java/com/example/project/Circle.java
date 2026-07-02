cf34c746e1b996e1bd9b01a999bca9e114d04959
miguelalejandrosalcas <miguelalejandrosalcas@gmail.com>
2026-07-02 12:44:42 -0500

    Update Circle.java

--- src/main/java/com/example/project/Circle.java
+++ src/main/java/com/example/project/Circle.java
@@ -3,13 +3,21 @@ package com.example.project;
 public class Circle implements Shape {
     // TODO: add fields as needed
 
+    private double radius;
+
     public Circle(double radius) {
         // TODO: store parameter(s)
+        this.radius =  radius;
     }
 
     @Override
     public double getArea() {
         // TODO: compute and return area
-        return 0;
+        if (radius <= 0) {
+            return 0;
+        } else {
+            System.out.println("El área del círculo es: ");
+            return Math.PI * Math.pow(radius, 2);
+        }
     }
 }
